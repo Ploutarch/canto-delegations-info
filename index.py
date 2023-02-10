0281821e046e57f4fbcfe15c407bb20fdb6bf7fe
Ploutarch <ploutarchnft@gmail.com>
2023-02-10 08:57:44 +0000

    Update index.py

--- index.py
+++ index.py
@@ -14,25 +14,40 @@ for full_val in only_val:
     val_addresses.append(val_adr)
 
 print("Finding all delegations...")
-delegations = []
+delegations_data = []
 for adr in val_addresses:
     command = os.popen('cantod q staking delegations-to ' + adr)
-    delegations_for_validator = command.read()
-    only_del = delegations_for_validator.split("delegation_responses:")[
+    delegations = command.read()
+    only_del = delegations.split("delegation_responses:")[
         1].split("\n- balance:")[1:]
 
     for dele in only_del:
+        indvidual_delegation = [adr for adr in dele.split(
+            "\n") if "amount" in adr][0].strip().split("amount: ")[1]
+
         del_adr = [adr for adr in dele.split(
             "\n") if "delegator_address" in adr][0].strip().split("delegator_address: ")[1]
         amount = int([adr for adr in dele.split("\n") if "amount" in adr]
                      [0].strip().split("amount: ")[1].replace('"', ""))
-        delegations.append([adr, del_adr, amount])
+
+        delegations_data.append([val_adr, del_adr, amount])
+
+print("Calculating total stake...")
+evm_delegations_data = []
+for data in delegations_data:
+    command = os.popen('cantod debug addr ' + data[1])
+    addresses = command.read()
+    evm_address = [adr for adr in addresses.split(
+        "\n") if "Address (EIP-55)" in adr][0].strip().split("Address (EIP-55): ")[1]
+
+    evm_delegations_data.append([data[0], evm_address, data[2]])
 
 print("Writing to staking.csv...")
 with open("staking.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(["Validator Address", "Delegator Address", "Tokens Staked"])
-    writer.writerows(delegations)
+    writer.writerows(evm_delegations_data)
 
 print("Done!")
+
 
