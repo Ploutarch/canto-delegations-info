c8b515513d8d477a4d8357a1cdbcb1b179b97ce9
Ploutarch <ploutarchnft@gmail.com>
2023-02-10 07:36:57 +0000

    Update index.py

--- index.py
+++ index.py
@@ -9,26 +9,37 @@ only_val = full_validators.split("validators:")[1].split("\n- commission:")[1:]
 
 val_addresses = []
 for full_val in only_val:
-val_adr = [adr for adr in full_val.split("\n") if "operator_address" in adr][0].strip().split("operator_address: ")[1]
-val_addresses.append(val_adr)
+    val_adr = [adr for adr in full_val.split(
+        "\n") if "operator_address" in adr][0].strip().split("operator_address: ")[1]
+    val_addresses.append(val_adr)
 
 print("Finding all delegations...")
 user_delegations = {}
+validator_delegations = []
 for adr in val_addresses:
-command = os.popen('cantod q staking delegations-to ' + adr)
-delegations = command.read()
-only_del = delegations.split("delegation_responses:")[1].split("\n- balance:")[1:]
+    command = os.popen('cantod q staking delegations-to ' + adr)
+    delegations = command.read()
+    only_del = delegations.split("delegation_responses:")[
+        1].split("\n- balance:")[1:]
 
-rew = 0
-for dele in only_del:
-    del_adr = [adr for adr in dele.split("\n") if "delegator_address" in adr][0].strip().split("delegator_address: ")[1]
-    amount = int([adr for adr in dele.split("\n") if "amount" in adr][0].strip().split("amount: ")[1].replace('"', ""))
+    for dele in only_del:
+        indvidual_delegation = [adr for adr in dele.split(
+            "\n") if "amount" in adr][0].strip().split("amount: ")[1]
+        del_adr = [adr for adr in dele.split(
+            "\n") if "delegator_address" in adr][0].strip().split("delegator_address: ")[1]
+        amount = int([adr for adr in dele.split("\n") if "amount" in adr]
+                     [0].strip().split("amount: ")[1].replace('"', ""))
 
-    user_delegations.append([adr, del_adr, amount])
+        validator_delegations.append([adr, del_adr, amount])
+
+print("Calculating total stake...")
+
 print("Writing to staking.csv...")
 with open("staking.csv", "w", newline="") as f:
-writer = csv.writer(f)
-writer.writerow(["Validator", "Delegator", "Amount"])
-writer.writerows(user_delegations)
+    writer = csv.writer(f)
+    writer.writerow(['Validator', 'Delegator Address', 'Amount of Token Delegated'])
+    writer.writerows(validator_delegations)
 
 print("Done!")
+
+
